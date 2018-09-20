c5cf2698b15d5fc1bab5ecbfb40734120395183f
Timafei <33371343+TimaG13@users.noreply.github.com>
2018-09-20 14:49:42 +0300

    fixed

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,6 @@
-
 $(document).ready(function () {
 
-    $(window).on('mousemove',function (e) {
+    $(window).on('mousemove', function (e) {
         var w = $(window).width();
         var h = $(window).height();
 
@@ -9,17 +8,17 @@ $(document).ready(function () {
         var offsetX = 0.5 - e.pageX / w;
         var offsetY = 0.5 - e.pageY / h;
 
-        $('.parallax').each(function (i,el) {
+        $('.parallax').each(function (i, el) {
             var offset = parseInt($(el).data('offset'));
 
             var translate = 'translate3d(' + Math.round(offsetX * offset)
-            + 'px,' + Math.round(offsetY * offset) + 'px, 0px)' ;
+                + 'px,' + Math.round(offsetY * offset) + 'px, 0px)';
 
             $(el).css({
-                '-webkit-transform':translate,
-                '-moz-transform':translate,
-                '-o-transform':translate,
-                'transform':translate
+                '-webkit-transform': translate,
+                '-moz-transform': translate,
+                '-o-transform': translate,
+                'transform': translate
             })
 
         });
@@ -33,10 +32,17 @@ $(document).ready(function () {
     // slider
     $('.center').slick({
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ]
     });
     $(".slick-next.slick-arrow").remove()
-
 
 
 });
